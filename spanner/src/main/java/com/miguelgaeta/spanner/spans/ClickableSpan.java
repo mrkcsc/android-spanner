d2426c33699ea5f91fad6a6b4b13efc9dc6cc9a2
mrkcsc <miguel@miguelgaeta.com>
2016-09-18 23:28:31 -0700

    Remove color from clickable span.

--- spanner/src/main/java/com/miguelgaeta/spanner/spans/ClickableSpan.java
+++ spanner/src/main/java/com/miguelgaeta/spanner/spans/ClickableSpan.java
@@ -1,12 +1,7 @@
 package com.miguelgaeta.spanner.spans;
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.text.TextPaint;
 import android.view.View;
-
-import static com.miguelgaeta.spanner.spans._ColorCompat.getColor;
-import static com.miguelgaeta.spanner.spans._ColorCompat.getColorAttrResId;
 
 /**
  * Created by Miguel Gaeta on 9/17/16.
@@ -16,38 +11,19 @@ public class ClickableSpan extends android.text.style.ClickableSpan {
 
     private final View.OnClickListener onClickListener;
     private final boolean underline;
-    private final int color;
 
-    public ClickableSpan(final View.OnClickListener onClickListener, final int color, final boolean underline) {
+    public ClickableSpan(final View.OnClickListener onClickListener, final boolean underline) {
         this.onClickListener = onClickListener;
         this.underline = underline;
-        this.color = color;
     }
 
-    public ClickableSpan(final View.OnClickListener onClickListener, final int color) {
-        this(onClickListener, color, false);
-    }
-
-    public ClickableSpan(final View.OnClickListener onClickListener, final Context context, final int colorResId, final boolean underline) {
-        this(onClickListener, getColor(context, colorResId), underline);
-    }
-
-    public ClickableSpan(final View.OnClickListener onClickListener, final Context context, final int colorResId) {
-        this(onClickListener, getColor(context, colorResId));
-    }
-
-    public ClickableSpan(final View.OnClickListener onClickListener, final Resources.Theme theme, final int colorAttrResId, final boolean underline) {
-        this(onClickListener, getColorAttrResId(theme, colorAttrResId), underline);
-    }
-
-    public ClickableSpan(final View.OnClickListener onClickListener, final Resources.Theme theme, final int colorAttrResId) {
-        this(onClickListener, getColorAttrResId(theme, colorAttrResId), false);
+    public ClickableSpan(final View.OnClickListener onClickListener) {
+        this(onClickListener, true);
     }
 
     @Override
     public void updateDrawState(final TextPaint drawState) {
         drawState.setUnderlineText(underline);
-        drawState.setColor(color);
     }
 
     @Override
