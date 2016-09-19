94cfe64ddc60eed8d386afea2bec4b65054e7543
mrkcsc <miguel@miguelgaeta.com>
2016-09-18 21:09:32 -0700

    Fix some regex groupings.

--- spanner/src/main/java/com/miguelgaeta/spanner/SpannerMarkdown.java
+++ spanner/src/main/java/com/miguelgaeta/spanner/SpannerMarkdown.java
@@ -18,12 +18,16 @@ import java.util.regex.Pattern;
 @SuppressWarnings({"DefaultFileTemplate", "WeakerAccess"})
 public class SpannerMarkdown {
 
-    private static final String REGEX_CODE_BLOCK = "((```)([\\s\\S]*?)(```))|((`)([\\s\\S]*?)(`))";
+    private static final String REGEX_CODE_BLOCK = "((```)([\\s\\S]*?)(```))";
+    private static final String REGEX_CODE_INLINE = "((`)([\\s\\S]*?)(`))";
+    private static final String REGEX_CODE = REGEX_CODE_BLOCK + "|" + REGEX_CODE_INLINE;
     private static final String REGEX_UNDERLINE = "(__)([\\s\\S]*?)(__)";
     private static final String REGEX_BOLD_ITALIC = "(\\*\\*\\*)([\\s\\S]*?)(\\*\\*\\*)";
     private static final String REGEX_BOLD = "(\\*\\*)([\\s\\S]*?)(\\*\\*)";
     private static final String REGEX_ITALIC = "((\\*)([\\s\\S]*?)(\\*))|((_)([\\s\\S]*?)(_)($|\\s))";
     private static final String REGEX_STRIKE_THROUGH = "(~~)([\\s\\S]*?)(~~)";
+
+    // TODO: At least one middle char.
 
     private static final Pattern PATTERN_CODE_BLOCK = Pattern.compile(REGEX_CODE_BLOCK);
     private static final Pattern PATTERN_UNDERLINE = Pattern.compile(REGEX_UNDERLINE);
@@ -35,21 +39,21 @@ public class SpannerMarkdown {
     private static final Spanner.MatchStrategy STRATEGY_UNDERLINE = new Spanner.MatchStrategy(PATTERN_UNDERLINE, new Spanner.OnMatchListener() {
         @Override
         public Spanner.Replacement call(Matcher matcher) {
-            return new Spanner.Replacement(matcher.group(), new UnderlineSpan());
+            return new Spanner.Replacement(matcher.group(2), new UnderlineSpan());
         }
     });
 
     private static final Spanner.MatchStrategy STRATEGY_BOLD_ITALIC = new Spanner.MatchStrategy(PATTERN_BOLD_ITALIC, new Spanner.OnMatchListener() {
         @Override
         public Spanner.Replacement call(Matcher matcher) {
-            return new Spanner.Replacement(matcher.group(), new BoldItalicSpan());
+            return new Spanner.Replacement(matcher.group(2), new BoldItalicSpan());
         }
     });
 
     private static final Spanner.MatchStrategy STRATEGY_BOLD = new Spanner.MatchStrategy(PATTERN_BOLD, new Spanner.OnMatchListener() {
         @Override
         public Spanner.Replacement call(Matcher matcher) {
-            return new Spanner.Replacement(matcher.group(), new BoldSpan());
+            return new Spanner.Replacement(matcher.group(2), new BoldSpan());
         }
     });
 
@@ -70,12 +74,17 @@ public class SpannerMarkdown {
     private static final Spanner.MatchStrategy STRATEGY_STRIKE_THROUGH = new Spanner.MatchStrategy(PATTERN_STRIKE_THROUGH, new Spanner.OnMatchListener() {
         @Override
         public Spanner.Replacement call(Matcher matcher) {
-            return new Spanner.Replacement(matcher.group(), new StrikethroughSpan());
+            return new Spanner.Replacement(matcher.group(2), new StrikethroughSpan());
         }
     });
 
-    public static Spanner.MatchStrategy forCodeBlock(final OnCodeBlockMatchListener onMatchListener) {
-        return new Spanner.MatchStrategy(PATTERN_CODE_BLOCK, new Spanner.OnMatchListener() {
+    public static Spanner.MatchStrategy forCodeInline() {
+
+        return null;
+    }
+
+    public static Spanner.MatchStrategy forCode(final OnCodeBlockMatchListener onMatchListener) {
+        return new Spanner.MatchStrategy(REGEX_CODE, new Spanner.OnMatchListener() {
             @Override
             public Spanner.Replacement call(Matcher matcher) {
                 final boolean block = matcher.group(1) != null;
